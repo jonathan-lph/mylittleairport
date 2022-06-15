ecf3a5b7740e4aa9eef01148f97bf395c0eeb40d
jonathan-lph <jonathan.lph@hotmail.com>
2022-06-16 01:37:13 +0800

    Changed fonts for English

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@400;700&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&family=Noto+Serif+SC:wght@400;700&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />
