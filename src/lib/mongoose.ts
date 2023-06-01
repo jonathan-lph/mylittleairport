8da3fdd5818c5cad622de573dec1ce70dcb929c3
jonathan-lph <jonathan.lph@hotmail.com>
2023-06-01 15:27:24 +0100

    Merged from main

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,9 +1,11 @@
 import mongoose from "mongoose"
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
-}
+// if (!process.env.MONGODB_URI) {
+//   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
+// }
 
-const mongoosePromise = mongoose.connect(process.env.MONGODB_URI)
+const mongoosePromise = process.env.MONGODB_URI
+  ? mongoose.connect(process.env.MONGODB_URI)
+  : null
 
 export default mongoosePromise
